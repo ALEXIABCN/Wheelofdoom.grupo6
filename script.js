530695896b440e359c775c4b8e5fcad96c3025e7
Kristina Maier <kaudiking@gmail.com>
2021-11-10 15:23:23 +0100

    sdsd

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
 
-
+//la foto esta oculta
     $(".wonderwoman-img").hide();
 
+//  ponemos el texto del botton. Html-escribe este texto
+    $("#button").html('Elegir Coder');
+
+//  Clickamos del botton 
     $("#button").click(function(){
+// click ocultamos la lista de las coders
         $(".lista-coders").hide(); 
-         
+// click mostramos imagen
         $(".wonderwoman-img").show();
-    
+//   cambiamos el texto del botton 
+     $("#button").html('Volver a la lista');
+
+//  volvemos a inicio
+
+
+
+
       });
 
 
