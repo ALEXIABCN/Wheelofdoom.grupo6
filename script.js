5f71bbc32f51b0874145a18d3bb9236e675a3260
Kristina Maier <kaudiking@gmail.com>
2021-11-10 14:54:21 +0100

    tersero

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+
+    $(".wonderwoman-img").hide();
+
+    $("#button").click(function(){
+        $(".lista-coders").hide(); 
+         
+        $(".wonderwoman-img").show();
+    
+      });
+
+
+});
+
+
+
+
