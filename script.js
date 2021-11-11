59f9985413511d97fddc2a0c405cbcfefaa8c4a7
Marisa Posada <marisa.posada@gmail.com>
2021-11-11 23:00:24 +0100

    lista de coders agregada desde Javascript y filtrado funcionando en console log

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 $(document).ready(function () {
 
   
-    let coders=["Gràcia","Marisa","Alexia","Kristina","Alisa","Ana C.","Anna G.","Candy","Carmen","Desirée","Faby","Gabrielle","Hellen","Joana","Judith","Laura C.","Laura M.","Rosa","Sandra","Sara","Sonia","Tamara","Valentina","Yuliya"]      
-    let winner=coders[Math.floor(Math.random()*coders.length)];
+    let coders=["Gràcia","Marisa","Alexia","Kristina","Alisa","Ana C.","Anna G.","Candy","Carmen","Desirée","Faby","Gabrielle","Hellen","Joana","Judith","Laura C.","Laura M.","Rosa","Sandra","Sara","Sonia","Tamara","Valentina","Yuliya"];    
     
+    
+    let winner=coders[Math.floor(Math.random()*coders.length)];
+
+    
+    coders.forEach(eachCoder);
+    function eachCoder(elemento) {
+        $("#lista-coders ul").append( "<li>" + elemento + "</li>");
+    }
+
+    let codersListFiltered = coders.filter(function(coder) {
+      return coder !== winner; 
+    });
+
     $(".wonderwinner").hide();//la foto esta oculta por decto
+    
 
       function elegirCoder(){
         $(".lista-coders").hide(); // ocultamos la lista de las coders
@@ -17,8 +30,9 @@ $(document).ready(function () {
         $(".wonderwinner").hide();//la foto esta oculta por decto
         $("#button").text('Elegir Coder'); //el texto por defecto
         $("#lista-coders").show();
+        console.log(codersListFiltered) ;
       }
-
+      
       
       $("#button").click(function(){
           if($("#lista-coders").is(":visible")){
